a6a7a39de0def9e84b3a506f696b5c752871b97d
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-07-13 22:56:10 +0200

    init

---
+++ static/js/app.js
@@ -0,0 +1,255 @@
+document.addEventListener("DOMContentLoaded", function() {
+  /**
+   * HomePage - Help section
+   */
+  class Help {
+    constructor($el) {
+      this.$el = $el;
+      this.$buttonsContainer = $el.querySelector(".help--buttons");
+      this.$slidesContainers = $el.querySelectorAll(".help--slides");
+      this.currentSlide = this.$buttonsContainer.querySelector(".active").parentElement.dataset.id;
+      this.init();
+    }
+
+    init() {
+      this.events();
+    }
+
+    events() {
+      /**
+       * Slide buttons
+       */
+      this.$buttonsContainer.addEventListener("click", e => {
+        if (e.target.classList.contains("btn")) {
+          this.changeSlide(e);
+        }
+      });
+
+      /**
+       * Pagination buttons
+       */
+      this.$el.addEventListener("click", e => {
+        if (e.target.classList.contains("btn") && e.target.parentElement.parentElement.classList.contains("help--slides-pagination")) {
+          this.changePage(e);
+        }
+      });
+    }
+
+    changeSlide(e) {
+      e.preventDefault();
+      const $btn = e.target;
+
+      // Buttons Active class change
+      [...this.$buttonsContainer.children].forEach(btn => btn.firstElementChild.classList.remove("active"));
+      $btn.classList.add("active");
+
+      // Current slide
+      this.currentSlide = $btn.parentElement.dataset.id;
+
+      // Slides active class change
+      this.$slidesContainers.forEach(el => {
+        el.classList.remove("active");
+
+        if (el.dataset.id === this.currentSlide) {
+          el.classList.add("active");
+        }
+      });
+    }
+
+    /**
+     * TODO: callback to page change event
+     */
+    changePage(e) {
+      e.preventDefault();
+      const page = e.target.dataset.page;
+
+      console.log(page);
+    }
+  }
+  const helpSection = document.querySelector(".help");
+  if (helpSection !== null) {
+    new Help(helpSection);
+  }
+
+  /**
+   * Form Select
+   */
+  class FormSelect {
+    constructor($el) {
+      this.$el = $el;
+      this.options = [...$el.children];
+      this.init();
+    }
+
+    init() {
+      this.createElements();
+      this.addEvents();
+      this.$el.parentElement.removeChild(this.$el);
+    }
+
+    createElements() {
+      // Input for value
+      this.valueInput = document.createElement("input");
+      this.valueInput.type = "text";
+      this.valueInput.name = this.$el.name;
+
+      // Dropdown container
+      this.dropdown = document.createElement("div");
+      this.dropdown.classList.add("dropdown");
+
+      // List container
+      this.ul = document.createElement("ul");
+
+      // All list options
+      this.options.forEach((el, i) => {
+        const li = document.createElement("li");
+        li.dataset.value = el.value;
+        li.innerText = el.innerText;
+
+        if (i === 0) {
+          // First clickable option
+          this.current = document.createElement("div");
+          this.current.innerText = el.innerText;
+          this.dropdown.appendChild(this.current);
+          this.valueInput.value = el.value;
+          li.classList.add("selected");
+        }
+
+        this.ul.appendChild(li);
+      });
+
+      this.dropdown.appendChild(this.ul);
+      this.dropdown.appendChild(this.valueInput);
+      this.$el.parentElement.appendChild(this.dropdown);
+    }
+
+    addEvents() {
+      this.dropdown.addEventListener("click", e => {
+        const target = e.target;
+        this.dropdown.classList.toggle("selecting");
+
+        // Save new value only when clicked on li
+        if (target.tagName === "LI") {
+          this.valueInput.value = target.dataset.value;
+          this.current.innerText = target.innerText;
+        }
+      });
+    }
+  }
+  document.querySelectorAll(".form-group--dropdown select").forEach(el => {
+    new FormSelect(el);
+  });
+
+  /**
+   * Hide elements when clicked on document
+   */
+  document.addEventListener("click", function(e) {
+    const target = e.target;
+    const tagName = target.tagName;
+
+    if (target.classList.contains("dropdown")) return false;
+
+    if (tagName === "LI" && target.parentElement.parentElement.classList.contains("dropdown")) {
+      return false;
+    }
+
+    if (tagName === "DIV" && target.parentElement.classList.contains("dropdown")) {
+      return false;
+    }
+
+    document.querySelectorAll(".form-group--dropdown .dropdown").forEach(el => {
+      el.classList.remove("selecting");
+    });
+  });
+
+  /**
+   * Switching between form steps
+   */
+  class FormSteps {
+    constructor(form) {
+      this.$form = form;
+      this.$next = form.querySelectorAll(".next-step");
+      this.$prev = form.querySelectorAll(".prev-step");
+      this.$step = form.querySelector(".form--steps-counter span");
+      this.currentStep = 1;
+
+      this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
+      const $stepForms = form.querySelectorAll("form > div");
+      this.slides = [...this.$stepInstructions, ...$stepForms];
+
+      this.init();
+    }
+
+    /**
+     * Init all methods
+     */
+    init() {
+      this.events();
+      this.updateForm();
+    }
+
+    /**
+     * All events that are happening in form
+     */
+    events() {
+      // Next step
+      this.$next.forEach(btn => {
+        btn.addEventListener("click", e => {
+          e.preventDefault();
+          this.currentStep++;
+          this.updateForm();
+        });
+      });
+
+      // Previous step
+      this.$prev.forEach(btn => {
+        btn.addEventListener("click", e => {
+          e.preventDefault();
+          this.currentStep--;
+          this.updateForm();
+        });
+      });
+
+      // Form submit
+      this.$form.querySelector("form").addEventListener("submit", e => this.submit(e));
+    }
+
+    /**
+     * Update form front-end
+     * Show next or previous section etc.
+     */
+    updateForm() {
+      this.$step.innerText = this.currentStep;
+
+      // TODO: Validation
+
+      this.slides.forEach(slide => {
+        slide.classList.remove("active");
+
+        if (slide.dataset.step == this.currentStep) {
+          slide.classList.add("active");
+        }
+      });
+
+      this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 6;
+      this.$step.parentElement.hidden = this.currentStep >= 6;
+
+      // TODO: get data from inputs and show them in summary
+    }
+
+    /**
+     * Submit form
+     *
+     * TODO: validation, send data to server
+     */
+    submit(e) {
+      e.preventDefault();
+      this.currentStep++;
+      this.updateForm();
+    }
+  }
+  const form = document.querySelector(".form--steps");
+  if (form !== null) {
+    new FormSteps(form);
+  }
+});
---
+++ templates/form-confirmation.html
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <header class="header--form-page">
+      <nav class="container container--70">
+        <ul class="nav--actions">
+          <li class="logged-user">
+            Witaj Agata
+            <ul class="dropdown">
+              <li><a href="#">Profil</a></li>
+              <li><a href="#">Moje zbiórki</a></li>
+              <li><a href="#">Wyloguj</a></li>
+            </ul>
+          </li>
+        </ul>
+
+        <ul>
+          <li><a href="index.html" class="btn btn--without-border active">Start</a></li>
+          <li><a href="index.html#steps" class="btn btn--without-border">O co chodzi?</a></li>
+          <li><a href="index.html#about-us" class="btn btn--without-border">O nas</a></li>
+          <li><a href="index.html#help" class="btn btn--without-border">Fundacje i organizacje</a></li>
+          <li><a href="index.html#contact" class="btn btn--without-border">Kontakt</a></li>
+        </ul>
+      </nav>
+
+      <div class="slogan container container--90">
+          <h2>
+            Dziękujemy za przesłanie formularza Na maila prześlemy wszelkie
+            informacje o odbiorze.
+          </h2>
+      </div>
+    </header>
+
+    <footer>
+      <div class="contact">
+        <h2>Skontaktuj się z nami</h2>
+        <h3>Formularz kontaktowy</h3>
+        <form class="form--contact">
+          <div class="form-group form-group--50">
+            <input type="text" name="name" placeholder="Imię" />
+          </div>
+          <div class="form-group form-group--50">
+            <input type="text" name="surname" placeholder="Nazwisko" />
+          </div>
+
+          <div class="form-group">
+            <textarea
+              name="message"
+              placeholder="Wiadomość"
+              rows="1"
+            ></textarea>
+          </div>
+
+          <button class="btn" type="submit">Wyślij</button>
+        </form>
+      </div>
+      <div class="bottom-line">
+        <span class="bottom-line--copy">Copyright &copy; 2018</span>
+        <div class="bottom-line--icons">
+          <a href="#" class="btn btn--small"
+            ><img src="images/icon-facebook.svg"
+          /></a>
+          <a href="#" class="btn btn--small"
+            ><img src="images/icon-instagram.svg"
+          /></a>
+        </div>
+      </div>
+    </footer>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
